c58a820cbb69254aee52e7ded80e7d9e9b7b7115
EmersonQS <51520684+EmersonQS@users.noreply.github.com>
2020-06-08 20:29:57 -0500

    master
    Android

---
+++ BoticaV2/app/src/main/java/com/example/boticav2/activity_crearusuario.java
@@ -0,0 +1,14 @@
+package com.example.boticav2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_crearusuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crearusuario);
+    }
+}
